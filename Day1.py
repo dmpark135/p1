cbe14f3f9bcaeb694c61af62235736235d303fb7
dave <dmpark135@gmail.com>
2021-02-28 20:23:37 -0500

    Initial commit

---
+++ Day1.py
@@ -0,0 +1,5 @@
+
+url = 'https://github.com/dmpark135/Advent.git'
+data = read(url)
+
+print (data)
---
+++ Talkingaboutpractice.py
@@ -0,0 +1,8 @@
+name = 'david'
+print (f'Hello {name}, would you like to learn some python today?')
+print (name.upper())
+print (name.lower())
+print (name.title())
+famous= ' albert einstein '
+message = (f'{famous.title()} \n \t once said "a person"')
+print(message)
