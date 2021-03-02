a46d4dada4e74a77656c0e5792f48737c85d8b04
dave <dmpark135@gmail.com>
2021-03-01 23:49:39 -0500

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,4 +1,8 @@
 names = ['ace', 'bees','cds']
-print (f'{names[0]} hola man')
-print (f'{names[1]} hola man')
-print (f'{names[2]} hola man')
+print (f'{names[0]} please join us for dinner')
+print (f'{names[1]} please join us for dinner')
+print (f'{names[2]} please join us for dinner')
+
+names.remove('ace')
+names.append('bob')
+print (names)
