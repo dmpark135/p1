c2a8ff45967eb77955370f6ecb7798319c39672f
dave <dmpark135@gmail.com>
2021-03-03 11:10:57 -0500

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,25 +1,15 @@
-names = ['ace', 'bees','cds']
-print (f'{names[0]} please join us for dinner')
-print (f'{names[1]} please join us for dinner')
-print (f'{names[2]} please join us for dinner')
-
-x="ace"
-names.remove(x)
-print(names)
-print(x)
-
-names.append('bob')
-print (names)
-print('we found more space')
-names.insert(0,'xeno')
-names.insert(int(len(names)/2),'fox' )
-names.append('hero')
-print(names)
-
-print(f'{names.pop()} sorry your not invited')
-print(f'{names.pop()} sorry your not invited')
-print(f'{names.pop()} sorry your not invited')
-print(f'{names.pop()} sorry your not invited')
-print(f'{names[0]} your still invited')
-del names[0:2]
-print(names)
+visit = ['korea', 'spain', 'italy','brazil','japan']
+print(visit)
+print(sorted(visit))
+print(visit)
+print(sorted(visit,reverse=True))
+print(visit)
+visit.reverse()
+print(visit)
+visit.reverse()
+print(visit)
+visit.sort()
+print(visit)
+visit.sort(reverse= True)
+print(visit)
+print (len(visit))
