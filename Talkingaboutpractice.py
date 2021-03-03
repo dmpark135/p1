ff4f6808bfcb2cacbbd2ee9796818b79640d43c3
dave <dmpark135@gmail.com>
2021-03-02 23:35:53 -0500

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -3,6 +3,23 @@ print (f'{names[0]} please join us for dinner')
 print (f'{names[1]} please join us for dinner')
 print (f'{names[2]} please join us for dinner')
 
-names.remove('ace')
+x="ace"
+names.remove(x)
+print(names)
+print(x)
+
 names.append('bob')
 print (names)
+print('we found more space')
+names.insert(0,'xeno')
+names.insert(int(len(names)/2),'fox' )
+names.append('hero')
+print(names)
+
+print(f'{names.pop()} sorry your not invited')
+print(f'{names.pop()} sorry your not invited')
+print(f'{names.pop()} sorry your not invited')
+print(f'{names.pop()} sorry your not invited')
+print(f'{names[0]} your still invited')
+del names[0:2]
+print(names)
