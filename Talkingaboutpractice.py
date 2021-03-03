6dfb0e5a37888f28350adccbe7df47370a8698ee
dave <dmpark135@gmail.com>
2021-03-03 16:13:31 -0500

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,15 +1,14 @@
-visit = ['korea', 'spain', 'italy','brazil','japan']
-print(visit)
-print(sorted(visit))
-print(visit)
-print(sorted(visit,reverse=True))
-print(visit)
-visit.reverse()
-print(visit)
-visit.reverse()
-print(visit)
-visit.sort()
-print(visit)
-visit.sort(reverse= True)
-print(visit)
-print (len(visit))
+pizzas = ['cheese', 'mushroom', 'works']
+
+for piz in pizzas:
+    print(f"I like {piz.title()} pizza")
+    
+print("I really like Pizza")
+
+Animals = ["lion", "monkey", "penguine"]
+for An in Animals:
+    print(f"A {An} would make a great pet")
+print("any of these animals would make a great pet!")
+
+
+    
