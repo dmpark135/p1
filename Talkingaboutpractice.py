3bb25549f788e31486e8666df3253ca567f78730
dave <dmpark135@gmail.com>
2021-04-02 09:33:55 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -15,3 +15,13 @@ pets =[p1,p2,p3]
 
 for x in pets:
     print(x)
+
+favorite_places = {
+        'ace':['seoul','usa','hk'],
+        'brian':['vietnam','africa','london'],
+        'dan':['china','oc','la']
+        }
+for key,value in favorite_places.items():
+    print(f'This dude {key.title()}, loves getting chicks from:')
+    for y in value:
+        print(f'\t{y.title()}')
