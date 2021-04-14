4a63930356114d059d17ec7f291dcd8ea4dd1092
dave <dmpark135@gmail.com>
2021-04-14 10:31:37 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,20 +1,10 @@
-dictx= {}
-
-nums = True
-while nums:
-    name = input('whats your name boy?')
-    response= input('where yo headed')
+def make_shirt(size = 'L',text = 'I love python!'):
+    '''size and text to print'''
+    print(size,text)
     
-    dictx[name] = response
-    
-    repeat= input('would you like to let the next person respond? (yes/no)')
-    if repeat == 'no':
-        nums = False
-        
-print('\n-- Poll Results--')
-for name,response in dictx.items():
-    print(f'{name} is my name, and I\'m headed to {response}')
-    
+make_shirt()
+make_shirt(size = 'm')
+make_shirt(size = 's', text = 'what da')
 
         
 
