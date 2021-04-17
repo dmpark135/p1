999533b33d238d367b114ea5bfb3cc32a57be9be
dave <dmpark135@gmail.com>
2021-04-17 12:04:07 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,15 +1,25 @@
-def make_shirt(size = 'L',text = 'I love python!'):
-    '''size and text to print'''
-    print(size,text)
-    
-make_shirt()
-make_shirt(size = 'm')
-make_shirt(size = 's', text = 'what da')
+def make_album(name,title, songs = None):
+    x = {'name':name, 'title': title}
+    if songs:
+        x['songs']= songs
+    return x
 
-        
-
+while True:
+    print('\nPlease tell me the name of the album: ')
+    print('(enter \'q\' at any time to quit)')
     
-        
-        
-        
-        
+    n = input("name of artist: ")
+    if n =='q':
+        break
+    
+    t = input("title of album: ")
+    if t == 'q':
+        break
+    
+    s = input("songs name: ")
+    if s == 'q':
+        break
+    
+    info = make_album(n, t, s)
+    print(f'\nHello, {info}')
+
