d5a4118c34886190a4bb19dcccf17905ea62791d
dave <dmpark135@gmail.com>
2021-04-20 13:01:03 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,25 +1,8 @@
-def make_album(name,title, songs = None):
-    x = {'name':name, 'title': title}
-    if songs:
-        x['songs']= songs
-    return x
+z = ['sup','ho','bleh']
 
-while True:
-    print('\nPlease tell me the name of the album: ')
-    print('(enter \'q\' at any time to quit)')
+def show_messages(x):
+    for word in x:
+        y = f'word: {word.title()}'
+        print(y)
     
-    n = input("name of artist: ")
-    if n =='q':
-        break
-    
-    t = input("title of album: ")
-    if t == 'q':
-        break
-    
-    s = input("songs name: ")
-    if s == 'q':
-        break
-    
-    info = make_album(n, t, s)
-    print(f'\nHello, {info}')
-
+show_messages(z)
