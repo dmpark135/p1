b641ab5c5b687346f2a6e0935ea1c9955a779ec4
dave <dmpark135@gmail.com>
2021-04-25 20:28:49 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,8 +1,25 @@
 z = ['sup','ho','bleh']
-
+sent_messages = []
 def show_messages(x):
     for word in x:
         y = f'word: {word.title()}'
         print(y)
+        
+def send_messages(x):
+    while x:
+        y = x.pop()
+        print(f'here you go foo {y}')
+        sent_messages.append(y)
+send_messages(z[:])
+print(sent_messages)
+print(z)
+
+
+        
+                
     
-show_messages(z)
+
+        
+    
+        
+        
