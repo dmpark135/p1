19ee0befb80dd23bd810d06b03db42ccaf387409
dave <dmpark135@gmail.com>
2021-04-26 15:31:32 -0400

    .
    .

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,25 +1,2 @@
-z = ['sup','ho','bleh']
-sent_messages = []
-def show_messages(x):
-    for word in x:
-        y = f'word: {word.title()}'
-        print(y)
-        
-def send_messages(x):
-    while x:
-        y = x.pop()
-        print(f'here you go foo {y}')
-        sent_messages.append(y)
-send_messages(z[:])
-print(sent_messages)
-print(z)
-
-
-        
-                
-    
-
-        
-    
-        
-        
+import printing_functions as pf
+pf.printing_models('lskjfls')
---
+++ printing_functions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 15:10:04 2021
+
+@author: agoge
+"""
+
+def printing_models(x):
+    print(x)
+    
