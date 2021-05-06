7cc62ae80a1d71a4881d683848715d8dc9a107c4
dave <dmpark135@gmail.com>
2021-05-06 10:29:09 -0400

    Update Talkingaboutpractice.py

--- Talkingaboutpractice.py
+++ Talkingaboutpractice.py
@@ -1,32 +1,20 @@
-class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type, served= 0):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-        self.served = served
-        
-    def describe_restaurant(self):
-        print(f'name of restaurant is {self.restaurant_name}.')
-        print(f'dish specialty is {self.cuisine_type}.')
-        
-    def set_number_served(self):
-        print(f'You been served {self.served}')
-        
-    def increment_number_served(self, given):
-        if given >= self.served:
-            self.served = given
-        else:
-            print(f'too much {self.served}')
+class User:
+    def __init__(self, first_name, last_name, *xyz):
+        self.first_name= first_name
+        self.last_name= last_name
+        self.xyz = xyz
         
         
         
+    def describe_user(self, xyz):
+        print(f'{first_name}, {last_name} {xyz}')        
+ 
+    def greet_user(self):
+        print(f'Hello {self.first_name} {self.last_name}')
         
-    def open_restaurant(self):
-        print(f'{self.restaurant_name} is open')
-        
-xyz= Restaurant('mama mia','spagatti', served = 80)
-xyz.describe_restaurant()
-xyz.open_restaurant()
-xyz.set_number_served()
+mike = User('mike', 'lee', 'ace')
+print(f'Name : {mike.first_name} {mike.last_name} {mike.xyz}')
 
-xyz.increment_number_served(90)
-xyz.set_number_served()
+        
+    
+    
